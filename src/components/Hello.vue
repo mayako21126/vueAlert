fa7e59cd54a722e147a7c4e7dd7f554bc199538e
rukinosaki <302719631@qq.com>
2017-02-28 10:42:58 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  :root {
+    --mainColor: red;
+    --danger-theme: {
+      color: white;
+      border:1px solid sienna;
+    };
+  }
+  a{
+    color: var(--mainColor);
+  }
+  span {
+    @apply --danger-theme;
+  }
+</style>
